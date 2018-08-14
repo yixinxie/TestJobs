fdc920c61a0c38ef572d13b9ba63bc6cf828e871
yixinxie <calvine_xie@hotmail.com>
2018-08-14 21:33:33 +0800

    2

--- Assets/JobTest/sim_oop/Belt.cs
+++ Assets/JobTest/sim_oop/Belt.cs
@@ -4,8 +4,74 @@ using UnityEngine;
 namespace Simulation_OOP {
     public class Belt : MonoBehaviour {
         public BeltData target;
+        public List<Vector3> path;
+        public MeshFilter mfilter;
         private void Awake() {
-            
+            path = new List<Vector3>(16);
+        }
+        public static void setPath(Vector3[] positions, float smoothness) {
+            int steps = 10;
+            Vector3[] ctrlPos;
+            Vector3[] four = new Vector3[4];
+            setPathPoints(positions, smoothness, out ctrlPos);
+            Vector3 lastleft = Vector3.zero;
+            Vector3 lastright = Vector3.zero;
+            for (int j = 0; j < ctrlPos.Length / 2; ++j) {
+                four[0] = positions[j];
+                four[1] = ctrlPos[j * 2];
+                four[2] = ctrlPos[j * 2 + 1];
+                four[3] = positions[j + 1];
+                Vector3 left, right;
+               
+                Vector3 lastPos = BezierStatic.evalBezier(four[0], four[1], four[2], four[3], 0);
+                for (int i = 1; i <= steps; i++) {
+                    float t = (float)i / (float)steps;
+                    Vector3 thisPos = BezierStatic.evalBezier(four[0], four[1], four[2], four[3], t);
+                    Debug.DrawLine(lastPos, thisPos, Color.black);
+                    Vector3 dir = thisPos - lastPos;
+                    dir.Normalize();
+                    dir = Vector3.Cross(dir, Vector3.up);
+                    dir.Normalize();
+                    //dir *=
+                    right = thisPos - dir;
+                    left = thisPos + dir;
+                    if (i == 1 && j == 0) {
+                        lastright = lastPos - dir;
+                        lastleft = lastPos + dir;
+                    }
+
+                    Debug.DrawLine(lastleft, left, Color.green);
+                    Debug.DrawLine(lastright, right, Color.green);
+                    lastleft = left;
+                    lastright = right;
+                    lastPos = thisPos;
+
+                }
+            }
+
+        }
+        public static void setPathPoints(Vector3[] pts, float smoothness, out Vector3[] controlPts) {
+            controlPts = new Vector3[(pts.Length - 1) * 2];
+            Vector3 curPos = pts[0];
+            Vector3 nextPos = pts[1];
+            Vector3 lastdiff = nextPos - curPos;
+            lastdiff.Normalize();
+            controlPts[0] = pts[0] + lastdiff * smoothness;
+            for (int i = 1; i < pts.Length - 1; ++i) {
+                curPos = pts[i];
+                nextPos = pts[i + 1];
+                Vector3 diff = nextPos - curPos;
+                diff.Normalize();
+                Vector3 avg = (diff + lastdiff) / 2f;
+                avg.Normalize();
+                controlPts[i * 2 - 1] = pts[i] - avg * smoothness;
+                controlPts[i * 2] = pts[i] + avg * smoothness;
+                lastdiff = diff;
+            }
+            lastdiff = pts[pts.Length - 1] - pts[pts.Length - 2];
+            controlPts[(pts.Length - 1) * 2 - 1] = pts[pts.Length - 1] - lastdiff * smoothness;
+
+            //mfilter.mesh.ver
         }
         
     }
--- Assets/NetworkingTest/Scripts/BezierStatic.cs
+++ Assets/NetworkingTest/Scripts/BezierStatic.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Simulation_OOP;
 public class BezierStatic : MonoBehaviour {
-    public Transform[] four;
+    public Transform[] targets;
+    Vector3[] positions;
+    Vector3[] four;
     public static Vector3 evalBezier(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t) {
         //Vector3 ret;
         //ret.x = Mathf.Pow(1 - t, 3f) * p0.x + 3f * Mathf.Pow(1 - t, 2f) * t * p1.x + 3f * (1 - t) * Mathf.Pow(t, 2f) * p2.x + 3 * Mathf.Pow(t, 3f) * p3.x;
@@ -15,20 +17,21 @@ public class BezierStatic : MonoBehaviour {
     }
 	// Use this for initialization
 	void Start () {
-		
-	}
+        positions = new Vector3[targets.Length];
+        four = new Vector3[4];
+    }
 
-    public float toVal = 1f;
+    public float smoothness = 1f;
     public int steps = 100;
 	// Update is called once per frame
 	void Update () {
-        Vector3 lastPos = evalBezier(four[0].position, four[1].position, four[2].position, four[3].position, 0);
-        for (int i = 1; i < steps; i++) {
-            float t = (toVal / (float)steps) * i;
-            Vector3 thisPos = evalBezier(four[0].position, four[1].position, four[2].position, four[3].position, t);
-            Debug.DrawLine(lastPos, thisPos, Color.green);
-            lastPos = thisPos;
 
+        for(int i = 0; i < targets.Length; ++i) {
+            positions[i] = targets[i].position;
         }
-	}
+        
+        Belt.setPath(positions, smoothness);
+
+    }
+    
 }
