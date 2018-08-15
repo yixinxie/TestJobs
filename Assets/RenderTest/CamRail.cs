dc5b5452f32d5fa2805c546bd09d927c9908a8b8
yixinxie <calvine_xie@hotmail.com>
2018-08-15 23:24:53 +0800

    1

---
+++ Assets/RenderTest/CamRail.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamRail : MonoBehaviour {
+    public Vector3 from;
+    public Vector3 to;
+    float lerpDir;
+    float alpha;
+    public float speed = 5f;
+	// Use this for initialization
+	void Start () {
+        lerpDir = 1f;
+        from = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        alpha += Time.deltaTime / speed * lerpDir;
+            
+        if(alpha > 1f) {
+            alpha = 1f;
+            lerpDir = -1f;
+        }
+        else if (alpha < 0f) {
+            alpha = 0f;
+            lerpDir = 1f;
+        }
+        transform.position = Vector3.Lerp(from, to, alpha);
+    }
+}
