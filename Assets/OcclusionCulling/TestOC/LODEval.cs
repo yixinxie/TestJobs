cbcc121f6316a7a58c07b0ba87a2d3a8e96e308e
yixinxie <calvine_xie@hotmail.com>
2018-09-10 19:01:55 +0800

    1

---
+++ Assets/OcclusionCulling/TestOC/LODEval.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LODEval : MonoBehaviour {
+    // static
+    Vector3[] positions;
+    float[] distances;
+    
+	// Update is called once per frame
+	void Update () {
+        Vector3 hotpos = Vector3.zero;
+		for(int i = 0; i < positions.Length; ++i) {
+            float dist = Vector3.Distance(hotpos, positions[i]);
+            if(dist < distances[i]) {
+
+            }
+        }
+	}
+}
---
+++ Assets/OcclusionCulling/TestOC/LODRendererInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LODRendererInfo : MonoBehaviour {
+    void OnBecameVisible() {
+        Debug.Log(name + " vis");
+    }
+    void OnBecameInvisible() {
+        Debug.Log(name + " invis");
+    }
+}
